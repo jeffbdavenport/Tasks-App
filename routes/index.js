75ad1ff06306a62a9fc758a45c6fea296209cfa2
Jeff <jeff@jeffreydvp.com>
2023-12-05 11:47:28 -0700

    Install mongoose

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,14 @@ router.get('/tasks/:id', function (req, res, next) {
   res.json({ tasks: [req.params.id] });
 });
 
+
+// POST Create a new task
+router.post('/tasks/create', function (req, res, next) {
+
+  console.log('Request Id:', req.params.id);
+
+  res.json({ tasks: [req.params.id] });
+
+  // res.status(500).send({ error: err });
+});
 module.exports = router;
